4dddaefdb642c6fd841897c175c5fa12d3765e23
Gautam Kotian <gautam.kotian@gmail.com>
2014-10-27 10:54:15 +0100

    fetch_pull_request: Make it meatier

--- fetch_pull_request.sh
+++ fetch_pull_request.sh
@@ -4,6 +4,13 @@
 #     $ fetchPullRequest 110
 #     gets PR#110 into a local branch called pr110
 
+# Confirm that we are in a git repository
+GIT_TOP=$(git rev-parse --show-toplevel)
+RC=$?
+if [ $RC != "0" ]; then
+    exit $RC
+fi
+
 # Make sure there are no modified files (as a hard reset will happen)
 NUM_MODIFIED_FILES=$(git diff --name-only | wc -l)
 if [ $NUM_MODIFIED_FILES != "0" ]; then
@@ -11,13 +18,40 @@ if [ $NUM_MODIFIED_FILES != "0" ]; then
     exit 1
 fi
 
-# TODO: confirm PR exists
+# Get the branch name
+if [ $# -eq 0 ]; then
+    BRANCH_NAME=$(git symbolic-ref --short HEAD 2>/dev/null)
+    if [ $? != "0" ]; then
+        echo "Cannot determine branch. Aborting."
+        exit 2
+    fi
 
-git remote update
+    # PR branches are named 'prNNN' where NNN is the PR number
+    if [[ ! $BRANCH_NAME =~ ^pr[0-9]{1,}$ ]]; then
+        echo "Branch $BRANCH_NAME is not a PR branch. Aborting."
+        exit 3
+    fi
+
+    # Get the PR number from the branch name
+    PR_NUM=${BRANCH_NAME:2}
+
+    # TODO: confirm PR exists
+elif [ $# -eq 1 ]; then
+    PR_NUM=$1
+
+    # TODO: confirm PR exists
+
+    BRANCH_NAME=pr$PR_NUM
+else
+    echo "Too many arguments. Aborting."
+    exit 4
+fi
+
+git remote update -p
 
 git checkout upstream/master
 
-git fetch upstream pull/$1/head
-git checkout pr$1 2>/dev/null || git checkout -b pr$1
+git fetch upstream pull/$PR_NUM/head
+git checkout $BRANCH_NAME 2>/dev/null || git checkout -b $BRANCH_NAME
 git reset --hard FETCH_HEAD
 
