9c5ccb42708296511b073a4fe7bb54eed7933e69
Gautam Kotian <gautam.kotian@gmail.com>
2016-04-11 19:22:49 +0200

    Add support for making individual file patches
    Still incomplete

--- list_files.sh
+++ list_files.sh
@@ -34,12 +34,22 @@ if [ $? != "0" ]; then
     exit 1
 fi
 
+COMMAND=$2
+
 FILES_LIST=(`git diff-tree --no-commit-id --name-only -r $COMMIT`)
 
-echo "Commit $COMMIT modifies the following ${#FILES_LIST[@]} files:"
+echo "Commit $COMMIT modifies ${#FILES_LIST[@]} files:"
 for (( i=0; i<${#FILES_LIST[@]}; ++i ))
 do
-    echo "    ${FILES_LIST[$i]}"
+    FILE_NAME=${FILES_LIST[$i]}
+    if [ -z "$COMMAND" ]; then
+        echo "    $FILE_NAME"
+    elif [ "$COMMAND" = "--file-patches" ]; then
+        # PATCH_FILE_NAME="$i"_"$COMMIT"_"$FILE_NAME"
+        # ORIG_PATCH_FILE=$(git format-patch -1 $COMMIT $FILE_NAME)
+        # mv $ORIG_PATCH_FILE $PATCH_FILE_NAME
+        echo "git format-patch -1 $COMMIT $FILE_NAME"
+    fi
 done
 
 exit 0
